e6277841ed8dd4e4e60a9d230cbfaf47b05d0320
Angelica dos Santos <65343425+angelcomp@users.noreply.github.com>
2022-08-06 16:41:38 -0300

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Angelica dos Santos on 06/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
