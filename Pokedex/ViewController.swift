44a8ded38983c63680c146e26da96ff019a25207
Tuanne Assenço <tuanne.assenco@gmail.com>
2022-08-07 11:51:52 -0300

    get source pokemons request

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -11,9 +11,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getPokemons()
+        
     }
+    
+    
+    func getPokemons() {
 
+        guard let url = URL(string: "https://pokeapi.co/api/v2/pokemon/?&limit=1154") else { return }
+
+        let session = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            guard let data = data else { return }
+            
+            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else { return }
+            
+            do {
+                let sourcePokemon = try JSONDecoder().decode(SourcePokemon.self, from: data)
+            }catch {
+                print("Decode error ):")
+            }
+        }
+        session.resume()
+    }
 
 }
 
